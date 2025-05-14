d507cda0abf7244c5cb672130ccd15e1bc21ebe4
Aung Chan Myae <aungchanmyae10997@gmail.com>
2025-05-14 15:14:29 +0700

    Making Table

---
+++ database/CRUD/create.js
@@ -0,0 +1,28 @@
+const { PrismaClient } = require ("@prisma/client")
+
+const prisma = new PrismaClient()
+
+
+prisma.user.create({
+    data:{
+        name: "Bob",
+        bio: "profile bio",
+        posts:{
+            create: [
+                {content: "First Post"},
+                {content: "Second Post"},
+                
+            ]
+        }
+    }
+})
+.then (() => {
+    console.log("Interested User Bob with Posts");
+})
+.catch( e => {
+    console.error(e);
+    process.exit(1);
+})
+.finally(() => {
+    prisma.$disconnect();
+})
