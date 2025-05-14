2b54f212908769d1feb471bb88858bd1ec3080d0
Aung Chan Myae <aungchanmyae10997@gmail.com>
2025-05-14 15:30:41 +0700

    prisma database

---
+++ database/CRUD/delete.js
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+prisma.post.deleteMany({
+    where: {userId: 1}
+}).then(() =>{
+    prisma.user.delete({
+        where: {id: 1},
+    }).then(() => {
+        console.log("Deleted User 1 and their posts");
+    });
+}).finally(() => {
+    prisma.$disconnect();
+})
+
