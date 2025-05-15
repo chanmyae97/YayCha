2f8cdeaa650cdd482c3030c8c8bd17b9f94bf1d6
Aung Chan Myae <aungchanmyae10997@gmail.com>
2025-05-15 11:54:27 +0700

    Express routes

---
+++ yaycha-api/routers/content.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+const prisma = require ("../prismaClient");
+
+router.get("/posts", async(req, res) =>{
+    try {
+        const data = await prisma.post.findMany({
+            include:{
+                user: true,
+                comments: true,
+            },
+            orderBy: {id : "desc"},
+            take: 20,
+        });
+
+        res.json(data);
+    } catch (e) {
+        res.status(500).json({ error: e});
+    }
+});
+
+module.exports = {contentRouter: router};
