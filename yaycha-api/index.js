d20381c1c28290f97bb1d739aa215a8e812ee2ec
Aung Chan Myae <aungchanmyae10997@gmail.com>
2025-05-15 14:29:00 +0700

    Fixing the env path error

--- yaycha-api/index.js
+++ yaycha-api/index.js
@@ -1,34 +1,35 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-const prisma = require('./prismaClient');
+const prisma = require("./prismaClient");
 
-const cors = require ("cors");
+const cors = require("cors");
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-const { contentRouter} = require("./routers/content");
+const { contentRouter } = require("./routers/content");
 app.use("/content", contentRouter);
 
+// const { userRouter } = require("./routers/user");
+// app.use("/", userRouter);
 
-app.get("/info", (req,res) =>{
-    res.json({msg: "Yaycha API"});
+app.get("/info", (req, res) => {
+	res.json({ msg: "Yaycha API" });
 });
 
-const server = app.listen(8000, () =>{
-    console.log("Yaycha API started at 8000...");
+const server = app.listen(1313, () =>{
+    console.log("Yaycha API started at 1313...");
 });
 
 const gracefulShutdown = async () => {
-    await prisma.$disconnect();
-    server.close(() => {
-        console.log("Yaycha API closed.")
-        process.exit(0);
-    })
-}
+	await prisma.$disconnect();
+	server.close(() => {
+		console.log("Yaycha API closed.");
+		process.exit(0);
+	});
+};
 
 process.on("SIGTERM", gracefulShutdown);
 process.on("SIGINT", gracefulShutdown);
